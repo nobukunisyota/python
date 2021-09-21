575210aa8a9eeb4eaad70a2f8cf79633758d197d
nobukunisyota <nobukuni.syota0727@gmail.com>
2021-09-21 23:48:02 +0900

    update : get_expenses_csv.py

--- analyze_expenses/get_expenses_csv.py
+++ analyze_expenses/get_expenses_csv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+from time import sleep
 import click
 from selenium import webdriver
 from dotenv import load_dotenv
@@ -16,9 +17,20 @@ DRIVER = webdriver.Chrome(DRIVER_PATH)
 def login():
     DRIVER.get(URL)
 
+def click_login_button():
+    element = DRIVER.find_element_by_link_text("Login")
+    element.click()
+
+def logout():
+    DRIVER.quit()
+
 @click.command()
 def cmd():
     login()
+    sleep(5)
+    click_login_button()
+    sleep(5)
+    logout()
 
 def main():
     cmd()
