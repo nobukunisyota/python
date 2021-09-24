7cf6d5efd58cdd05bda8dc59256d33098a21caee
nobukunisyota <nobukuni.syota0727@gmail.com>
2021-09-24 23:29:43 +0900

    work 210924

--- django/learning_quest/app/urls.py
+++ django/learning_quest/app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, LoginView, SignUpView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- django/learning_quest/app/views.py
+++ django/learning_quest/app/views.py
@@ -1,5 +1,16 @@
 from django.views.generic import TemplateView
 
 
+# Site Top
 class IndexView(TemplateView):
     template_name = 'templates/index.html'
+
+
+# Login
+class LoginView(TemplateView):
+    template_name = 'templates/login.html'
+
+
+# Sign up
+class SignUpView(TemplateView):
+    template_name = 'templates/signup.html'
