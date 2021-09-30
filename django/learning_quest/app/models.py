d67a56975d53e7e54f5ee763d0f78e0564b54a65
nobukunisyota <nobukuni.syota0727@gmail.com>
2021-10-01 00:12:21 +0900

    add reward model

--- django/learning_quest/app/models.py
+++ django/learning_quest/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class RewardTable(models.Model):
+    class Meta:
+        db_table = 'rewards'
+
+    reward = models.CharField(max_length=100)
+    total_time = models.IntegerField()
+    username = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.PROTECT,
+    )
+
+    def __str__(self):
+        return self.reward
+
