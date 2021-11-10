1765cf9aefb32a0d06dc4d808b2a98bca260c92f
nobukunisyota <nobukuni.syota0727@gmail.com>
2021-11-11 00:10:03 +0900

    paslog.py

--- original_module/paslog.py
+++ original_module/paslog.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import click
+import os
+import re
+import sys
+import tarfile
+import argparse
 
-def cmd():
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filename")
+    return parser.parse_args()
 
+def files(filename):
+    return os.listdir(filename)
+
+def decompress(filepath):
+    try:
+        with tarfile.open(filepath, 'w|') as tar:
+            tar.extractall(path='output')
+    except tarfile.TarError:
+        print("tar file open error occur")
+        pass
+
+
+def main():
+    args = get_args()
+    for file in files(args.filename):
+        m = re.match('.*tar.gz$', file)
+        if not m:
+            continue
+        decompress(os.path.join(os.getcwd(), file))
 
 if __name__ == "__main__":
     main()
